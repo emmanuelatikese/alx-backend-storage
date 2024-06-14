3b9a2e6e7d99f11efa7fea4b96a3a87dd0ee74bf
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-14 01:44:55 +0000

    file 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
--- creating user table
+-- creating user table with att
 -- the table has list of attribute
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY ,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
