17c43bba623fe4441ac6294fcb39b6f2ba69c970
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-15 12:34:20 +0100

    adding

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- triggering
+-- email validation
+
+DELIMITER //
+
+CREATE TRIGGER email_val
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+ IF NEW.email != OLD.email THEN
+    SET valid_email = 0;
+END IF;
+END //
+
+DELIMITER ;
