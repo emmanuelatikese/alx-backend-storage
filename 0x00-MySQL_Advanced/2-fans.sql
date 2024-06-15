c1f0db1370e5655e6594db06c3dda2df8162c588
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-15 06:27:54 +0100

    adding

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,4 +2,4 @@
 -- looking at origin and num_fan
 
 SELECT origin, SUM(fans) as `nb_fans` from metal_bands
-GROUP BY origin;
+GROUP BY origin ORDER BY DESC;
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- grouping and counting
+-- looking at band name and main style
+
+SELECT band_name, formed from metal_bands
+WHERE metal_bands.style = 'Glam rock';
