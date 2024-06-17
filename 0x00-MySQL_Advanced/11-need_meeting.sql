cddbefafda484914404573f9acf5ecd78f6986a2
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-17 13:06:16 +0100

    adding

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -5,4 +5,4 @@
 CREATE View need_meeting
 AS SELECT name FROM students
 WHERE score < 80 AND
-last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH;
+last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH);
