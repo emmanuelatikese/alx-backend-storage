b23b5d88a4440062483e671dc6151e28c317f6de
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-17 12:22:17 +0100

    adding

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,20 @@
+-- starting with function
+-- this is a div function
+
+
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE Ans INT;
+    IF b != 0 THEN
+        SET Ans = a / b;
+        RETURN Ans;
+    ELSE
+        RETURN 0;
+    END IF;
+END //
+
+DELIMITER ;
