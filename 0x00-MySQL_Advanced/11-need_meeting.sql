c5b0e77593151532db967a5b412b413549ba70f9
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-17 13:01:49 +0100

    adding

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,5 +2,5 @@
 -- it all about viewing
 
 
-CREATE View need_meeting AS SELECT score, last_meeting FROM students
+CREATE View need_meeting AS SELECT * FROM students
 WHERE score < 80 AND (last_meeting IS NULL OR DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
