9c380f6fe78658f40fe482c06fff257b302db49b
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-19 00:46:10 +0100

    adding

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,6 +5,12 @@
 def list_all(mongo_collection):
     '''function for sorting'''
     ans = mongo_collection.aggregate([
-        {"$project": {"name": "$name", "averageScore": {
-            "$avg": "$topics.score"}}}, {"$sort": {"averageScore": -1}}])
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
     return ans
