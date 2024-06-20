c33f3f065fa8660aa9458fab6fc2fc0e1a574249
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-20 03:01:07 +0100

    adding

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,6 +12,6 @@ class Cache:
 
     def store(self, data: Union[int, float, bytes, str]) -> str:
         '''creating a key'''
-        new_key = uuid1()
+        new_key = str(uuid1())
         self._redis.set(new_key, data)
-        return str(new_key)
+        return new_key
