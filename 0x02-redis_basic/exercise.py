b0906a069ada3b45b27f9b5c55d45236a7e27a0e
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-20 03:09:01 +0100

    adding

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 '''writing a class for redis'''
 import redis
-from uuid import uuid1
+from uuid import uuid4
 from typing import Union
 
 
 class Cache:
     '''the class is initialize'''
-    def __init__(self):
+    def __init__(self) -> None:
         '''Here goes nothing'''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[int, float, bytes, str]) -> str:
         '''creating a key'''
-        new_key = str(uuid1())
+        new_key = str(uuid4())
         self._redis.set(new_key, data)
         return new_key
