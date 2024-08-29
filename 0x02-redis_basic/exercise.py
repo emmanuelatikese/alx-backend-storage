f980bc6a45f4dea7b9d4ffec5904e40539c836c1
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-08-29 23:15:32 +0100

    adding files

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,8 +2,7 @@
 '''writing a class for redis'''
 import redis
 from uuid import uuid4
-from typing import Union
-
+from typing import Union, Optional, Callable
 
 class Cache:
     '''the class is initialize'''
@@ -17,3 +16,16 @@ class Cache:
         new_key = str(uuid4())
         self._redis.set(new_key, data)
         return new_key
+    
+    def get(self, key: str, fn: Callable = None) -> Union[str, float, bytes, int]:
+        '''gettting int or str'''
+        dt = self._redis.get(key)
+        return fn(dt) if fn else dt
+    
+    def get_str(self, key: str) -> str:
+        ''' getting str'''
+        return self._redis.get(key, lambda y: y.decode('utf-8'))
+    
+    def get_int(self, key: str) -> int:
+        '''getting int'''
+        return int(self._redis.get(key, lambda y: int(y)))
